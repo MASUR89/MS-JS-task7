16eaddaa02e5acd31ab00aca5d21c1f1ef626240
MASUR89 <surmava9@gmail.com>
2024-11-21 15:33:59 +0400

    final

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const text = "JavaScript does not seem very ease to me"
+
 const words = text.split(" ");
 console.log(words);
 
 for (let i = 0; i < words.length; i++){
-    if(words[i] >= 5) {
+    if(words[i].length >= 5) {
         words[i] = "LongWord"
     } else {
         words[i] = "ShortWord"
     }
 }
 console.log(words.join(" "));
+
+
+const text2 = text.split(" ");
+let count = 0;
+let i = 0;
+while (i < text2.length) {
+  if (text2[i].includes("a")) {
+    count++;
+  }
+  i++;
+}
+console.log(count);
+// ეს ნაწილი კარგად ვერ გავიგე
+
+
+const newText = text.split(" ")
+let index = 0;
+do {
+    console.log(newText[index]);
+    index++; 
+  } while (index < newText.length);
